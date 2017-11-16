828e29cd19c480f735924364620f6cb67e726706
Ubuntu <835943102@qq.com>
2017-11-16 20:45:13 +0800

    view

---
+++ src/ttms/view/JS/check.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lmy on 17-9-9.
+ */
+
+let user = "root", password = "123456";
+
+let checkUser = ()=> {
+    let userName = document.getElementById('user').value;
+    let passwordValue = document.getElementById('password').value;
+    if (userName == user && passwordValue == password) {
+        return true;
+    }
+    else {
+
+        alert('登陆失败！请检查用户名和密码');
+        return false;
+
+    }
+}
+
+
